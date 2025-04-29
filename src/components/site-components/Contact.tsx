493b172fa5f3f0f758d3c841355cf4097c7c31b6
Valtteri <savonen.emppu@gmail.com>
2025-04-29 19:40:09 +0300

    Added footer with tanstack credits

--- src/components/site-components/Contact.tsx
+++ src/components/site-components/Contact.tsx
@@ -2,64 +2,86 @@ import { Github, Linkedin } from "lucide-react";
 
 export function Contact() {
   return (
-    <section id="contact" className="section h-dvh flex items-center">
-      <div className="section w-full px-4 sm:px-8 md:px-16">
-        <div className="w-full max-w-7xl mx-auto">
-          <h2 className="text-center uppercase text-3xl sm:text-4xl md:text-6xl pt-6 md:pt-0 font-bold mb-6 sm:mb-10 md:mb-16 uppercase tracking-wide">
-            Contact
-          </h2>
+    <section id="contact" className="section h-dvh flex flex-col">
+      <div className="flex-grow flex items-center">
+        <div className="w-full px-4 sm:px-8 md:px-16">
+          <div className="w-full max-w-7xl mx-auto">
+            <h2 className="text-center uppercase text-3xl sm:text-4xl md:text-6xl pt-6 md:pt-0 font-bold mb-6 sm:mb-10 md:mb-16 uppercase tracking-wide">
+              Contact
+            </h2>
 
-          <div className="space-y-4 text-center text-base sm:text-lg md:text-xl text-gray-300">
-            <p>Any inquiries, collaborations, or just want to talk?</p>
+            <div className="space-y-4 text-center text-base sm:text-lg md:text-xl text-gray-300">
+              <p>Any inquiries, collaborations, or just want to talk?</p>
 
-            <a
-              href="mailto:valtteri@bittive.com"
-              className="underline inline-block hover:text-gray-400 transition-colors"
-            >
-              valtteri@bittive.com
-            </a>
-
-            <div className="flex space-x-6 justify-center pt-6">
               <a
-                href="https://x.com/vvaltterisa"
-                className="text-white hover:text-gray-400 transition-colors"
-                title="X / Twitter"
-                target="_blank"
+                href="mailto:valtteri@bittive.com"
+                className="underline inline-block hover:text-gray-400 transition-colors"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  viewBox="0 0 512 462.799"
-                  width="24"
-                  height="24"
+                valtteri@bittive.com
+              </a>
+
+              <div className="flex space-x-6 justify-center pt-6">
+                <a
+                  href="https://x.com/vvaltterisa"
+                  className="text-white hover:text-gray-400 transition-colors"
+                  title="X / Twitter"
+                  target="_blank"
                 >
-                  <path
-                    fill="currentColor"
-                    d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
-                  />
-                </svg>
-              </a>
-              <a
-                href="https://github.com/valtterisa"
-                className="text-white hover:text-gray-400 transition-colors"
-                title="GitHub"
-                target="_blank"
-              >
-                <Github size={24} />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/valtterisavonen/"
-                className="text-white hover:text-gray-400 transition-colors"
-                title="LinkedIn"
-                target="_blank"
-              >
-                <Linkedin size={24} />
-              </a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    viewBox="0 0 512 462.799"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
+                    />
+                  </svg>
+                </a>
+                <a
+                  href="https://github.com/valtterisa"
+                  className="text-white hover:text-gray-400 transition-colors"
+                  title="GitHub"
+                  target="_blank"
+                >
+                  <Github size={24} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/valtterisavonen/"
+                  className="text-white hover:text-gray-400 transition-colors"
+                  title="LinkedIn"
+                  target="_blank"
+                >
+                  <Linkedin size={24} />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <footer className="w-full mt-auto">
+        <div className="w-full px-4 sm:px-8 md:px-16">
+          <div className="max-w-7xl mx-auto text-center py-6">
+            <p className="text-sm sm:text-base text-gray-300 flex items-center justify-center gap-1">
+              Made with
+              <img src="/tanstack.png" width={20} height={20} />
+              <a
+                className="pr-1 font-bold hover:underline"
+                href="https://tanstack.com/start/latest"
+              >
+                TanStack{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-cyan-500">
+                  Start
+                </span>
+              </a>{" "}
+            </p>
+          </div>
+        </div>
+      </footer>
     </section>
   );
 }
